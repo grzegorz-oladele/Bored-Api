7e43a6fd1d08c37af0cfb28d8d12443b18b83c22
grzegorz_oladele <grzegorz.oladele@gmail.com>
2022-04-29 22:13:14 +0200

    Beans
    Creating RestTemplate and ObjectMapper as beans

--- src/main/java/pl/grzegorz/boredapi/BoredApiApplication.java
+++ src/main/java/pl/grzegorz/boredapi/BoredApiApplication.java
@@ -1,13 +1,26 @@
 package pl.grzegorz.boredapi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BoredApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoredApiApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
 	}
 
 }
