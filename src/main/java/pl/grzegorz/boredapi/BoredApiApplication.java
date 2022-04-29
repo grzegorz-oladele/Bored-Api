c4c8dee5c48a5421b868bc3dcb48bed91a9908d2
grzegorz_oladele <grzegorz.oladele@gmail.com>
2022-04-29 21:59:50 +0200

    Empty project
    Creating empty project

---
+++ src/main/java/pl/grzegorz/boredapi/BoredApiApplication.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.boredapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoredApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/grzegorz/boredapi/BoredApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.boredapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoredApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
