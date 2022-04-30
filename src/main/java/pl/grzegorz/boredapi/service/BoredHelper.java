2d3d61569d0e9e24135ca4f9cd6c41ed1fd349bf
grzegorz_oladele <grzegorz.oladele@gmail.com>
2022-04-30 10:47:36 +0200

    BoredHelper
    implementing checkPresenceActivity method

--- src/main/java/pl/grzegorz/boredapi/service/BoredHelper.java
+++ src/main/java/pl/grzegorz/boredapi/service/BoredHelper.java
@@ -1,2 +1,20 @@
-package pl.grzegorz.boredapi.service;public class BoredHelper {
+package pl.grzegorz.boredapi.service;
+
+import org.springframework.stereotype.Component;
+import pl.grzegorz.boredapi.exception.bored.BoredError;
+import pl.grzegorz.boredapi.exception.bored.BoredException;
+import pl.grzegorz.boredapi.model.entity.BoredEntity;
+
+import java.util.List;
+
+@Component
+public class BoredHelper {
+
+    protected void checkPresenceActivity(List<BoredEntity> activities, String key) {
+        for (BoredEntity activity : activities) {
+            if (activity.getKey().equals(key)) {
+                throw new BoredException(BoredError.BORED_ALREADY_EXISTS);
+            }
+        }
+    }
 }
