7f1c49753267b170e3fecd05b95aeea95badc55a
grzegorz_oladele <grzegorz.oladele@gmail.com>
2022-04-30 13:54:07 +0200

    LogController
    Refactoring

--- src/main/java/pl/grzegorz/boredapi/controller/LogController.java
+++ src/main/java/pl/grzegorz/boredapi/controller/LogController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.grzegorz.boredapi.model.entity.Log;
+import pl.grzegorz.boredapi.model.dto.LogDtoInfo;
 import pl.grzegorz.boredapi.service.log.LogService;
 
 import java.util.List;
@@ -17,7 +17,7 @@ public class LogController {
     private final LogService logService;
 
     @GetMapping
-    public List<Log> getAllLogs() {
+    public List<LogDtoInfo> getAllLogs() {
         return logService.getAllLogs();
     }
 }
