e5b79a3f6665de614e5d4e69e3e59070e5de0916
grzegorz_oladele <grzegorz.oladele@gmail.com>
2022-05-01 19:31:41 +0200

    Builder layer
    Creating documentation for builder layer

--- src/main/java/pl/grzegorz/boredapi/builder/ActivityLogDtoInfoBuilder.java
+++ src/main/java/pl/grzegorz/boredapi/builder/ActivityLogDtoInfoBuilder.java
@@ -2,21 +2,40 @@ package pl.grzegorz.boredapi.builder;
 
 import pl.grzegorz.boredapi.model.dto.ActivityLogDtoInfo;
 
+/**
+ * A class whose job is to build "activity log" objects using the BUILDER design pattern.
+ */
 public class ActivityLogDtoInfoBuilder {
 
     private long id;
     private String activityKey;
     private String creationDate;
 
+    /**
+     * @param id ID of the log object in the database
+     * @return object of type ActivityDtoInfoBuilder with completed ID parameter.
+     */
+
     public ActivityLogDtoInfoBuilder withId(long id) {
         this.id = id;
         return this;
     }
 
+    /**
+     * @param activityKey that has not been saved in the database due to duplication
+     * @return object of type ActivityDtoInfoBuilder with completed activityKey parameter.
+     */
+
     public ActivityLogDtoInfoBuilder withActivityKey(String activityKey) {
         this.activityKey = activityKey;
         return this;
     }
+
+    /**
+     * @param creationDate date and time when adding a new activity to the database was blocked due to
+     *                     activity duplication
+     * @return object of type ActivityLogDtoInfoBuilder with completed creationDate parameter.
+     */
 
     public ActivityLogDtoInfoBuilder withCreationDate(String creationDate) {
         this.creationDate = creationDate;
