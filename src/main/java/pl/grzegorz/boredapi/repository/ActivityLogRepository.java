fd273be94aec0fbe505e2352346310e0baeacd84
grzegorz_oladele <grzegorz.oladele@gmail.com>
2022-05-01 20:21:11 +0200

    Documentation - repository
    Creating documentation in the repository layer

--- src/main/java/pl/grzegorz/boredapi/repository/ActivityLogRepository.java
+++ src/main/java/pl/grzegorz/boredapi/repository/ActivityLogRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.grzegorz.boredapi.model.entity.ActivityLog;
 
+/**
+ * An interface that is the connection between the activity log table in the database and the application.
+ */
+
 @Repository
 public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
 }
--- src/main/java/pl/grzegorz/boredapi/repository/ActivityRepository.java
+++ src/main/java/pl/grzegorz/boredapi/repository/ActivityRepository.java
@@ -4,8 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.grzegorz.boredapi.model.entity.Activity;
 
+/**
+ * An interface that is the connection between the activity table in the database and the application.
+ */
+
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
+    /**
+     * The purpose of the method is to check if there is an activity in the database with the selected value "key"
+     * @param key activity key
+     * @return true if activity with specified "key" value exists in database
+     */
     boolean existsByKey(String key);
 }
